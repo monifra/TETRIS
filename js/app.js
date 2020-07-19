2d8f35d37ab52fe31a16f0d5171aaa520f394bf0
Monika Frankowska <monia.frankowska@gmail.com>
2020-07-19 15:07:12 +0200

    small changes in fies

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
 //Wait until HTML is loaded
 document.addEventListener('DOMContentLoaded', ()=> {
+
     //variables
     let box;
-    let DIV_FOR_ONE_BOX;
+    let divForOneBox;
+    const WIDTH = 10;
+
+
     //selecting DOM elements
     const BODY = document.getElementById('body');
     const LAYOUT = document.getElementById('layout');
+    const GRID = document.querySelector('.grid');
+    const SCORE = document.getElementById('score');
+    const START_BUTTON = document.getElementById('start-btn');
+
     //Create basic visual game structure
     //Append 200 DIVS
     for(let i = 0; i<200; i++){
-        DIV_FOR_ONE_BOX = document.createElement('div');
-        LAYOUT.appendChild(DIV_FOR_ONE_BOX);
+        divForOneBox = document.createElement('div');
+        LAYOUT.appendChild(divForOneBox);
         // console.log('One box');
     }
+
+    //create all squares array after they are append to DOM
+    const ALL_SQUARES = Array.from(document.querySelectorAll('.grid div'));
+    console.log(ALL_SQUARES);
 
 });
