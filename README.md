7e3602ef56ce56036fb4b22f04da1d002bf215e8
Monika Frankowska <monia.frankowska@gmail.com>
2020-07-20 13:35:56 +0200

    create random tetrimino

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 As a course is a basic javascript course, from the beginning I've decided to write some of the code on my own. I move some of the functionality from html to javascript to make it more adjustable. 
 1. Changed the rotations in tetrominos so they matches the original game rotation.
-2. Tetrominos initial spawn matches the one in original game.
+2. Tetrominos initial spawn matches the one in original game with the start rotation.
 ## More features
 1. Added the rest of tetriminos: z and j.
 ### TO DO: 
 1. Add game levels
-
+2. Maybe give tetrominos they own data file.
--- js/app.js
+++ js/app.js
@@ -4,79 +4,97 @@ document.addEventListener('DOMContentLoaded', ()=> {
     //variables
     let box;
     let divForOneBox;
-    const WIDTH = 10;
+    const width = 10;
 
 
     //selecting DOM elements
-    const BODY = document.getElementById('body');
-    const LAYOUT = document.getElementById('layout');
-    const GRID = document.querySelector('.grid');
-    const SCORE = document.getElementById('score');
-    const START_BUTTON = document.getElementById('start-btn');
+    const body = document.getElementById('body');
+    const layout = document.getElementById('layout');
+    const grid = document.querySelector('.grid');
+    const score = document.getElementById('score');
+    const startBtn = document.getElementById('start-btn');
 
     //the tetriminos
 
     const lTetrimino = [
-        [WIDTH, WIDTH+1, WIDTH+2,WIDTH*2+2],
-        [WIDTH*2, WIDTH*2 +1, WIDTH+1,1],
-        [WIDTH, WIDTH*2, WIDTH*2+1, WIDTH*2+2],
-        [1, WIDTH+1, WIDTH*2+1, 2]
+        [width, width+1, width+2,width*2+2],
+        [width*2, width*2 +1, width+1,1],
+        [width, width*2, width*2+1, width*2+2],
+        [1, width+1, width*2+1, 2]
     ];
 
     const jTetrimino = [
-        [WIDTH*2, WIDTH*2+1, WIDTH*2+2, WIDTH+2],
-        [1, WIDTH+1, WIDTH*2+1, WIDTH*2+2],
-        [WIDTH, WIDTH+1, WIDTH+2, WIDTH*2],
-        [0, 1, WIDTH+1, WIDTH*2+1]
+        [width*2, width*2+1, width*2+2, width+2],
+        [1, width+1, width*2+1, width*2+2],
+        [width, width+1, width+2, width*2],
+        [0, 1, width+1, width*2+1]
     ];
 
     const sTetrimino = [
-        [WIDTH+1, WIDTH+2, WIDTH*2, WIDTH*2+1],
-        [0, WIDTH, WIDTH+1, WIDTH*2+1],
-        [WIDTH+1, WIDTH+2, WIDTH*2, WIDTH*2+1],
-        [0, WIDTH, WIDTH+1, WIDTH*2+1]
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1]
     ];
 
     const zTetrimino = [
-        [WIDTH, WIDTH+1, WIDTH*2+1, WIDTH*2+2],
-        [2, WIDTH+2, WIDTH+1, WIDTH*2+1],
-        [WIDTH, WIDTH+1, WIDTH*2+1, WIDTH*2+2],
-        [2, WIDTH+2, WIDTH+1, WIDTH*2+1]
+        [width, width+1, width*2+1, width*2+2],
+        [2, width+2, width+1, width*2+1],
+        [width, width+1, width*2+1, width*2+2],
+        [2, width+2, width+1, width*2+1]
     ];
 
     const tTetrimino = [
-        [WIDTH, WIDTH+1, WIDTH+2, WIDTH*2+1],
-        [1, WIDTH, WIDTH+1, WIDTH*2+1],
-        [WIDTH+1, WIDTH*2, WIDTH*2+1, WIDTH*2+2],
-        [1, WIDTH+1, WIDTH+2, WIDTH*2+1]
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1],
+        [width+1, width*2, width*2+1, width*2+2],
+        [1, width+1, width+2, width*2+1]
     ];
 
     const oTetrimino = [
-        [0, 1, WIDTH, WIDTH+1],
-        [0, 1, WIDTH, WIDTH+1],
-        [0, 1, WIDTH, WIDTH+1],
-        [0, 1, WIDTH, WIDTH+1]
+        [width, width+1, width*2, width*2+1],
+        [width, width+1, width*2, width*2+1],
+        [width, width+1, width*2, width*2+1],
+        [width, width+1, width*2, width*2+1]
     ];
 
     const iTetrimino = [
-        [WIDTH, WIDTH+1, WIDTH+2, WIDTH+3],
-        [2, WIDTH+2, WIDTH*2+2, WIDTH*3+2],
-        [WIDTH, WIDTH+1, WIDTH+2, WIDTH+3],
-        [2, WIDTH+2, WIDTH*2+2, WIDTH*3+2]
+        [width, width+1, width+2, width+3],
+        [2, width+2, width*2+2, width*3+2],
+        [width, width+1, width+2, width+3],
+        [2, width+2, width*2+2, width*3+2]
     ];
+
+    const theTetriminos = [lTetrimino, jTetrimino, sTetrimino, zTetrimino, tTetrimino, oTetrimino, iTetrimino];
 
     //Create basic visual game structure
     //Append 200 DIVS
     for(let i = 0; i<200; i++){
         divForOneBox = document.createElement('div');
-        LAYOUT.appendChild(divForOneBox);
+        layout.appendChild(divForOneBox);
         // console.log('One box');
     }
 
     //create all squares array after they are append to DOM
-    const ALL_SQUARES = Array.from(document.querySelectorAll('.grid div'));
-    console.log(ALL_SQUARES);
+    const squares = Array.from(document.querySelectorAll('.grid div'));
+    console.log(squares);
 
+    //Game
 
+    let currentPosition = 1;
+    let current = theTetriminos[0][0];
+
+    //randomly select a tetromino
+    const randomTetrimino = Math.floor(Math.random()*theTetriminos.length);
+    console.log(randomTetrimino);
+    //Draw the first rotation in the first tetromino
+
+    const draw = () => {
+        current.forEach( index => {
+            squares[currentPosition + index].classList.add('tetrimino');
+        });
+    };
+
+    draw();
 
 });
